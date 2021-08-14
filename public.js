8dfa8e677ae64bcffb2c631a1b0e32d5f06e28bf
AlienGao <385184602@qq.com>
2021-08-14 20:33:58 +0800

    update

--- public.js
+++ public.js
@@ -1,7 +1,7 @@
 const git = require('simple-git')
 const colors = require('colors')
 const path = 'dist'
-const repo = 'https://github.com/xiao555/Resume.git'
+const repo = "git@github.com:AlienGao/resume.git";
 
 console.log(`Start public to your git repo[${repo}]\nPlease wait ...`.green)
 git(path)
@@ -12,10 +12,10 @@ git(path)
   .push(['-f', 'origin', 'master'], () => {
     console.log("Push to master success");
   })
-  .checkoutLocalBranch('gh-pages', () => {
-    console.log('Checkout to branch gh-pages');
-  })
-  .push(['-f', 'origin', 'gh-pages'])
-  .checkout('master', () => {
-    console.log('Finish public, back to branch master.');
-  })
+  // .checkoutLocalBranch('gh-pages', () => {
+  //   console.log('Checkout to branch gh-pages');
+  // })
+  // .push(['-f', 'origin', 'gh-pages'])
+  // .checkout('master', () => {
+  //   console.log('Finish public, back to branch master.');
+  // })
